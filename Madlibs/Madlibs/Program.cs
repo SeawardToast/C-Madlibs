a11a93e4d29d004cd58b8cd74e34a75275ad8680
Aidan Murphy <aidan@murphyhouse.us>
2015-12-04 23:16:47 -0500

    Finished dialouge pre mad lib todo: write mad lib questions and stories

--- Madlibs/Madlibs/Program.cs
+++ Madlibs/Madlibs/Program.cs
@@ -8,38 +8,91 @@ namespace Madlibs
 {
     class Program
     {
-        public String myString;
-        public String yourString;
-        public String aString;
-        public String answer;
+        public string playerName;
+        public string favoriteColor;
+        public string city;
+        public string smartAnswer;
+        public string gameAnswer;
+        public string libAnswer;
 
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello, what's your name?");
-            string myString = Console.ReadLine();
-            Console.WriteLine("Hello {0}! That's a cool name, what's your favorite color?", myString);
-            string yourString =  Console.ReadLine();
-            Console.WriteLine("Your name is {0} and your favorite color is {1}!  (press enter to continue)", myString, yourString);
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Hello {0}! That's a cool name, what's your favorite color?", playerName);
+            string favoriteColor =  Console.ReadLine();
+            Console.WriteLine("Your name is {0} and your favorite color is {1}!  (press enter to continue)", playerName, favoriteColor);
             Console.ReadLine();
             Console.WriteLine("What city do you live in?");
-            string aString = Console.ReadLine();
-            Console.WriteLine("{0}! That is a great place to live!  (press enter to continue)", aString);
+            string city = Console.ReadLine();
+            Console.WriteLine("{0}! That is a great place to live!  (press enter to continue)", city);
             Console.ReadLine();
-            Console.WriteLine("So let me see.. Your name is {0}, your favorite color is {1}, and you live in {2}. Do you think I'm smart? yes/no", myString, yourString, aString);
-            string answer = Console.ReadLine();
-            if (answer == "yes")
+            Console.WriteLine("So let me see.. Your name is {0}, your favorite color is {1}, and you live in {2}. Do you think I'm smart? yes/no", playerName, favoriteColor, city);
+            string smartAnswer = Console.ReadLine();
+            if (smartAnswer == "yes")
             {
                 Console.WriteLine("So do I... Hey! Do you want to play a game? yes/no");
+                string gameAnswer = Console.ReadLine();
+
+                if (gameAnswer == "yes")
+                {
+                    Console.WriteLine("Awesome! Have you ever done a mad lib before? yes/no");
+                    string libAnswer = Console.ReadLine();
+
+                    if (libAnswer == "yes")
+                    {
+                        Console.WriteLine("Okay, let's begin.  (press enter)");
+                        //goto
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, so here's some instructions. I will ask you to enter a series of nouns, adajectives, and verbs. You can be");
+                        Console.WriteLine("as creative as you would like! Once I have asked you all the questions, you can choose to have either mad lib number");
+                        Console.WriteLine("one, two, or three. I will then write you a short story involving all of your answers to my questions!  (press enter)");
+                        Console.ReadLine();
+                        //goto
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Awe that sucks.. Come back when you want to play!");
+                    Console.WriteLine("Game Over  (press enter)");
+                    Console.ReadLine();
+                    Environment.Exit(1); 
+                }
             }
             else
             {
                 Console.WriteLine("Well I think I am! Let me prove it.. Want to play a game? yes/no");
+                string gameAnswer = Console.ReadLine();
+
+                if (gameAnswer == "yes")
+                {
+                    Console.WriteLine("Awesome! Have you ever done a mad lib before? yes/no");
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.WriteLine("Awe that sucks.. Come back when you want to play!");
+                    Console.WriteLine("Game Over  (press enter)");
+                }
+                
+
             }
-            Console.ReadLine();
-             
+            //start madlib
+            Madlib();
+
 
 
         }
+
+        static void Madlib()
+        {
+            Console.WriteLine("We're in baby");
+            Console.ReadLine();
+        }
+
     }
 }
